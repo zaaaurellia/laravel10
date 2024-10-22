a428e09445ba8235060d9ed87bda981aad1f5fb6
zaaaurellia <aliqaarifina@gmail.com>
2024-10-22 12:45:14 +0700

    test

---
+++ app/Models/Product.php
@@ -0,0 +1,19 @@
+//app/Models/Product.php
+
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+use HasFactory;
+
+protected $fillable = [
+'title',
+'price',
+'product_code',
+'description'
+];
+}
